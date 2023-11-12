ce391d5d23634391a2b7409ca8869d4ed23c567b
Julia Nelz <121945980+I-asked@users.noreply.github.com>
2023-11-12 17:51:02 +0100

    Add fixed elf2e32 to optional tools

---
+++ lib/era/install-fixed-elf2e32.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+(( _IN_EPOCSTRAP ))
+
+>&2 echo -e ' ==> Downloading fixed elf2e32 sources'
+_clone_or_pull "${SYMBIAN_BUILD_REPO:-https://github.com/SymbianRevive/elf2e32.git}" elf2e32
+>&2 echo -e ' ==> Installing fixed elf2e32'
+&>/dev/null pushd elf2e32/group/
+  >&2 sbs -k -q --jobs "${MAKEJOBS}" -c tools2
+&>/dev/null popd
