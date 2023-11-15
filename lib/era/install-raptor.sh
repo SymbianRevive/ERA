decec6c5afb0d240c3494235d0fc8882a7e369ac
Julia Nelz <121945980+I-asked@users.noreply.github.com>
2023-11-15 14:33:45 +0100

    Fix escapes

--- lib/era/install-raptor.sh
+++ lib/era/install-raptor.sh
@@ -7,7 +7,7 @@ cat <<EOF >"${EPOCROOT}/epoc32/sbs_config/gcce.xml"
 <?xml version="1.0" encoding="ISO-8859-1"?>
 <build xmlns="http://symbian.com/xml/build" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://symbian.com/xml/build ../../schema/build/2_0.xsd">
     <var name="arm.changes">
-        <set name='VARIANT_HRH' value='$(EPOCINCLUDE)/variant/symbian_os.hrh'/>
+        <set name='VARIANT_HRH' value='\$(EPOCINCLUDE)/variant/symbian_os.hrh'/>
     </var>
 </build>
 EOF
@@ -16,7 +16,7 @@ cat <<EOF >"${EPOCROOT}/epoc32/sbs_config/root.xml"
 <?xml version="1.0" encoding="ISO-8859-1"?>
 <build xmlns="http://symbian.com/xml/build" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://symbian.com/xml/build ../../schema/build/2_0.xsd">
     <var name="root.changes">
-        <set name='VARIANT_HRH' value='$(EPOCINCLUDE)/variant/symbian_os.hrh'/>
+        <set name='VARIANT_HRH' value='\$(EPOCINCLUDE)/variant/symbian_os.hrh'/>
     </var>
 </build>
 EOF
